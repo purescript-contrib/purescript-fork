a1d075390c05cb431b4cfa96088e4c0cc2e42144
Gary Burgess <gary.burgess@gmail.com>
2016-11-21 19:45:16 +0000

    Parameterise error for canceler

--- src/Control/Monad/Fork/Canceler.purs
+++ src/Control/Monad/Fork/Canceler.purs
@@ -18,20 +18,19 @@ module Control.Monad.Fork.Canceler where
 
 import Prelude
 
-import Control.Monad.Eff.Exception (Error)
 import Data.Monoid (class Monoid)
 import Data.Newtype (class Newtype)
 
 -- | Semigroup (and monoid) for combining cancel functions produced by `fork`.
-newtype Canceler m = Canceler (Error → m Boolean)
+newtype Canceler e m = Canceler (e → m Boolean)
 
-derive instance newtypeCanceler ∷ Newtype (Canceler m) _
+derive instance newtypeCanceler ∷ Newtype (Canceler e m) _
 
-instance semigroupCanceler ∷ Apply m ⇒ Semigroup (Canceler m) where
+instance semigroupCanceler ∷ Apply m ⇒ Semigroup (Canceler e m) where
   append (Canceler f1) (Canceler f2) = Canceler (\e → (||) <$> f1 e <*> f2 e)
 
-instance monoidCanceler ∷ Applicative m ⇒ Monoid (Canceler m) where
+instance monoidCanceler ∷ Applicative m ⇒ Monoid (Canceler e m) where
   mempty = Canceler (const (pure false))
 
-cancel ∷ ∀ m. Canceler m → Error → m Boolean
+cancel ∷ ∀ e m. Canceler e m → e → m Boolean
 cancel (Canceler f) = f
