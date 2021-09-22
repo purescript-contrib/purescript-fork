dcc4b3744950db40ca5cbe1e7f2f126e0a68ae15
JordanMartinez <jordanalex.martinez@gmail.com>
2021-09-21 20:24:10 -0700

    Export module explicitly (as provided by compiler)

--- src/Control/Monad/Fork.purs
+++ src/Control/Monad/Fork.purs
@@ -5,7 +5,7 @@ module Control.Monad.Fork
 
 import Prelude
 
-import Control.Monad.Fork.Class as Class
+import Control.Monad.Fork.Class (class MonadBracket, class MonadFork, class MonadKill, BracketCondition(..), bracket, fork, join, kill, never, suspend, uninterruptible) as Class
 import Control.Monad.Fork.Class (class MonadBracket, BracketCondition(..), bracket)
 
 cancelWith ∷ ∀ err fiber m a. MonadBracket err fiber m ⇒ (err → m Unit) → m a → m a
